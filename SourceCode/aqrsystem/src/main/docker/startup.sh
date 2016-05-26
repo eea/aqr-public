38e12e2c3d5cbb2d180fce9ef30c911b6d3ed369
aliki kop <alk@eworx.gr>
2016-05-26 15:45:45 +0300

    Configuration to overwrite context.xml at startup #31468.

--- SourceCode/aqrsystem/src/main/docker/startup.sh
+++ SourceCode/aqrsystem/src/main/docker/startup.sh
@@ -4,6 +4,7 @@
 
 TMPCONF=/var/tmp/config
 mkdir -p $TMPCONF/WEB-INF/classes/META-INF
+mkdir -p $TMPCONF/META-INF
 
 sed -e "s#__DB_URL__#${DB_URL}#" \
     -e "s/__DB_PASSWORD__/${DB_PASSWORD}/" \
@@ -15,5 +16,11 @@ sed -e "s/__SERVER_PORT__/${SERVER_PORT:-80}/" \
     -e "s/__SERVER_NAME__/${SERVER_NAME}/" \
  /maven/ecas-config.properties > $TMPCONF/WEB-INF/classes/ecas-config.properties
 
+sed -e "s#__DB_URL__#${DB_URL}#" \
+    -e "s/__DB_PASSWORD__/${DB_PASSWORD}/" \
+    -e "s/__DB_USERNAME__/${DB_USERNAME}/" \
+    -e "s/__DB_DRIVER__/${DB_DRIVER}/" \
+ /maven/context.xml > $TMPCONF/META-INF/context.xml
+
 # Update the war file with the modifications and start Tomcat
-( cd $TMPCONF ; zip -r $CATALINA_HOME/webapps/ROOT.war WEB-INF ) && catalina.sh run
+( cd $TMPCONF ; zip -r $CATALINA_HOME/webapps/ROOT.war WEB-INF ; zip -r $CATALINA_HOME/webapps/ROOT.war META-INF ) && catalina.sh run
